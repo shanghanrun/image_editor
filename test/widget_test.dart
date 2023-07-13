b4e28ed497c543da69898b419ce5ee74c155f43e
shanghanrun <idim7@naver.com>
2023-07-13 18:01:05 +0900

    최종. 앞의 것과 비교가 필요하다. 특히  footer

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -9,11 +9,12 @@ import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
 import 'package:image_editor/main.dart';
+import 'package:image_editor/screen/home_screen.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(const MyApp());
+    await tester.pumpWidget(const MaterialApp(home: HomeScreen()));
 
     // Verify that our counter starts at 0.
     expect(find.text('0'), findsOneWidget);
